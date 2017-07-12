21ae2ee125e29fed3a8c2521252474ce6c19b990
philliprognerud <philliprognerud2@yahoo.com>
2017-07-12 04:15:56 +0000

    made some more changes

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"); 
-    
+
+//APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
@@ -15,9 +17,25 @@ var blogSchema = new mongoose.Schema({
     create: {type: Date, default: Date.now}
 });
 
+var Blog = mongoose.model("Blog", blogSchema);
 
 
+//RESTful ROUTES
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
 
+
+app.get("/blogs", function(req, res){
+   Blog.find({}, function(err, blogs){
+     if(err){
+         console.log(err);
+     } else {
+         res.render("index", {blogs: blogs}); 
+     }
+   });
+   
+});
 
 
 
