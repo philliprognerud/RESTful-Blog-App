2366fc99cab0a411864361159ca1a6d0cca48be9
philliprognerud <philliprognerud2@yahoo.com>
2017-07-12 02:54:22 +0000

    RESTful blog

---
+++ app.js
@@ -0,0 +1,26 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"); 
+    
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    create: {type: Date, default: Date.now}
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("server running"); 
+});
