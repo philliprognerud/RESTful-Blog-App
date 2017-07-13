8d96de7a9f24b6d0bad6004f28d149df2fd56109
philliprognerud <philliprognerud2@yahoo.com>
2017-07-13 02:44:23 +0000

    added edit and update routes

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"); 
+var express        = require("express"),
+    methodOverride = require("method-override"),
+    bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose"),
+    app            = express();
+
 
 //APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -60,6 +63,28 @@ app.get("/blogs/:id", function(req, res){
           res.redirect("/blogs");
       } else {
           res.render("show", {blog: foundBlog});
+      }
+   });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.render("edit", {blog:foundBlog});
+       }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+      if(err){
+          res.redirect("/blogs");
+      } else {
+          res.redirect("/blogs/" + req.params.id);
       }
    });
 });
